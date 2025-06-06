8d3d22ccaa778e7d379df879213ab0f60192809f
TheAlly <133992794+theallyprompts@users.noreply.github.com>
2025-06-06 19:42:09 -0400

    Fix resource path resolution

--- PixelPruner.py
+++ PixelPruner.py
@@ -16,7 +16,7 @@ def resource_path(relative_path):
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
     except Exception:
-        base_path = os.path.abspath(".")
+        base_path = os.path.abspath(os.path.dirname(__file__))
     
     return os.path.join(base_path, relative_path)
 
